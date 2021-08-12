610e0e72b0875b24269f209eb0e695f8c925f4d4
hhy <371254169@qq.com>
2021-08-12 23:40:34 +0800

    tijiao daimatest

--- mymodel01/src/main/java/helloWorld.java
+++ mymodel01/src/main/java/helloWorld.java
@@ -2,5 +2,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("nima");
+        System.out.println("mm");
     }
 }
