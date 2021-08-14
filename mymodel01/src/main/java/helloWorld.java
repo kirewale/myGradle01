fb7354ea16effd152d9f32dd3430c8bbbbf1c89a
hhy <371254169@qq.com>
2021-08-14 21:19:22 +0800

    hot-fix delete update

--- mymodel01/src/main/java/helloWorld.java
+++ mymodel01/src/main/java/helloWorld.java
@@ -1,6 +1,6 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworldsfasf");
         System.out.println("nima");
         System.out.println("mm");
         System.out.println("nimeide");
